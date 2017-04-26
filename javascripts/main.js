4292efd3f115b57199e176da50fb8762ce3f7c91
drworm75 <dwgames@gmail.com>
2017-04-25 19:33:03 -0500

    Addresses writing to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,55 @@
 $(document).ready(function(){
+
 	$('body').on('click', 'li', (e) => {
-		console.log(e.target.innerHTML);
-		loadPlaces(e.target.innerHTML).then((data) => {
-			console.log(data);
+		const type = e.target.innerHTML;
+		loadPlaces(type).then((results) => {
+			writeToDom(results);
 			}).catch((error) => {
 				console.log(error);
 			});
 		});
 
+	
+	$("body").on("click", ".place", (e) => {
+		let place_id = e.target.id;
+		console.log(place_id);
+		loadDetail(place_id).then((result) => {
+			writeAddressToDom(result.formatted_address);
+		});
 
-		const loadPlaces = (dropdownType) => {
-			return new Promise ((resolve, reject) => {
-				$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=
-					36.1627,86.7816&radius=500&type=${dropdownType}&key=${apiKey}`)
-			.done((data) => resolve(data))
+	});
+
+	const loadDetail = (place_id) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${apiKey}`)
+			.done((data) => resolve(data.result))
 			.fail((error) => reject(error));
-			});
-		};
+		});
+	};
+
+	const loadPlaces = (dropdownType) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=
+				36.174465,-86.767960&radius=50000&type=${dropdownType}&key=${apiKey}`)
+		.done((data) => resolve(data.results))
+		.fail((error) => reject(error));
+		});
+	};
+
+	const writeAddressToDom = (address) => {
+		let outputString = `<div>${address}</div>`;
+		$("#addresses").append(outputString);
+	};
+
+	const writeToDom = (results) => {
+		let outputString = "";
+		console.log(results.length);
+		for (let i = 0; i < results.length; i++) {
+			outputString += `<a href="#"><div id='${results[i].place_id}'class="place">${results[i].name}</div></a>`;
+		}
+		$('#input').html(outputString);
+		console.log(outputString);
+	};
 
 });
 
